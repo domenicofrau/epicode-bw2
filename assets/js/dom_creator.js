e538abd32150b6d4519ac00b02fd834ef33c6359
Riccardo Calvi <riccardoclv@gmail.com>
2022-12-14 13:50:03 +0100

    JS => Rifattorizzato JS

--- assets/js/dom_creator.js
+++ assets/js/dom_creator.js
@@ -1,8 +1,25 @@
-let nomeUtente = document.getElementById("profilo")
-nomeUtente.innerHTML = `<img src="https://picsum.photos/200" class="d-inline rounded-circle" alt="profilo_img" id="profilo_img">${window.localStorage.getItem("nome")}`
 
-let asideSX = document.getElementById("left")
-asideSX.innerHTML += `<div id="sticky">
+let amici = creaAmico(creaUtente())
+
+function Utente(nome, traccia, artista, album, tempo){
+    this.nome = nome;
+    this.traccia = traccia;
+    this.artista = artista;
+    this.album = album;
+    this.tempo = tempo;
+}
+
+function creaUtente() {
+    let out = []
+    out.push(new Utente("Marco", "viola", "fedez", "Viola", "3 ore"))
+    out.push(new Utente("Antonio", "viola", "fedez", "Viola", "3 ore"))
+    out.push(new Utente("Luca", "viola", "fedez", "Viola", "3 ore"))
+    out.push(new Utente("Giovanni", "viola", "fedez", "Viola", "3 ore"))
+    return out
+}
+
+function creaSidebarSX(params) {
+    document.getElementById("left").innerHTML += `<div id="sticky">
 
     <section id="menu_sinistra">
         <nav>
@@ -38,37 +55,58 @@ asideSX.innerHTML += `<div id="sticky">
             <li><a href=""></a>lorem</li>
         </ul>
     </section>
-</div>`
+</div>`  
+}
 
-let asideDX = document.getElementById("right")
-asideDX.innerHTML += `<div id="sticky">
-
-<!----- Head Amico ----->
-<section id="amico_head" class="d-flex justify-content-between">
-    <h3 class="d-inline">Attività Amici</h3>
-    <span>
-        <i class="bi bi-person-add"></i>
-        <i class="bi bi-x"></i>
-    </span>
-</section>
-
-<!----- Amici ----->
-<section>
-
-    <!----- Amico ----->
-    <div class="amico container d-flex">
-        <div>
-            <img src="assets/img/39425.jpg" alt="" class="amico_img rounded-circle">
-        </div>
-        <div>
-            <h4 class="amico_nome m-0">Charlie Hookham</h4>
-            <p class="amico_titolo m-0">in Camera • Yumi Zouma</p>
-            <i class="bi bi-disc"></i><span class="amico_album ml-1">EP III</span>
-        </div>
-        <div>
-            <p class="amico_tempo">4 ore</p>
-        </div>
+function creaSidebarDX(params) {
+    document.getElementById("right").innerHTML += `<div id="sticky">
+    
+    <!----- Head Amico ----->
+    <section id="amico_head" class="d-flex justify-content-between">
+        <h3 class="d-inline">Attività Amici</h3>
+        <span>
+            <i class="bi bi-person-add"></i>
+            <i class="bi bi-x"></i>
+        </span>
+    </section>
+    
+    <!----- Amici ----->
+    <section>
+    
+    ${amici}
+        
+    </section>
     </div>
-</section>
-</div>
-`
+    `
+    
+}
+
+function creaAmico(utenti) {
+    let out
+    utenti.forEach(utente => {
+        console.log(utente)
+        out += `<!----- Amico ----->
+        <div class="amico container d-flex">
+            <div>
+                <img src="https://picsum.photos/200" alt="" class="amico_img rounded-circle">
+            </div>
+            <div>
+                <h4 class="amico_nome m-0">${utente.nome}</h4>
+                <p class="amico_titolo m-0">${utente.traccia} • ${utente.artista}</p>
+                <i class="bi bi-disc"></i><span class="amico_album ml-1">${utente.album}</span>
+            </div>
+            <div>
+                <p class="amico_tempo">4 ore</p>
+            </div>
+        </div>`
+    });
+    return out
+}
+  
+function cambiaUtente() {
+    document.getElementById("profilo").innerHTML = `<img src="https://picsum.photos/200" class="d-inline rounded-circle" alt="profilo_img" id="profilo_img">${window.localStorage.getItem("nome")}`
+}
+
+cambiaUtente()
+creaSidebarSX()
+creaSidebarDX()
